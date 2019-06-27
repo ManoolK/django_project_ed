892364027605f2fbae4f1038b9a10037772cf332
Suvorova O <mail.suvorova.o@gmail.com>
2019-06-27 15:47:45 -0700

    Added authentication and publishing posts.

--- mydj/settings.py
+++ mydj/settings.py
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -121,6 +122,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
