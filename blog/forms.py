710dfdce2aa0eb67abe2907b29e7ea262ae6d3ef
Suvorova O <mail.suvorova.o@gmail.com>
2019-06-27 17:16:35 -0700

    Creared simple comments system

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 __author__ = "suvorovao"
 
@@ -10,3 +10,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text')
